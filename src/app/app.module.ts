f20835fb00da260eb6afe6eb933de6e1f212c0fe
JUANDATM <kira00711@hotmail.com>
2020-01-28 17:05:00 -0600

    Casi Buscando Video 62

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { RouterModule } from '@angular/router';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +19,13 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    RouterModule
   ],
   providers: [
     HeroesService
---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { HeroesService } from '../../servicios/herores.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent {
+  heroe: any = {};
+
+  constructor( private avtivatedRoute: ActivatedRoute,
+               private _heroeService: HeroesService
+    ) {
+  this.avtivatedRoute.params.subscribe( params => {
+    this.heroe = this._heroeService.getHeroe( params['id'] );
+    console.log(params['id'] );
+
+  });
+
+   }
+
+}
